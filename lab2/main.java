010e3cdbbfa496aa23f71e5edc5270bdbd6c9da5
Matthew7064 <matthew.programowanie1@gmail.com>
2023-03-06 20:11:16 +0100

    Update function

--- lab2/main.java
+++ lab2/main.java
@@ -1,33 +1,36 @@
 import java.io.*;
 
-class lab0 {
+class lab2 {
     public static void my_printf(String format_string, String param){
-        for(int i=0;i<format_string.length();i++){
-            if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-                //System.out.print(param);
+        for(int i=0;i<format_string.length();i++) {
+            if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')) {
                 for(int j=0;j<param.length();j++) {
                     System.out.print(swapLetterCase(param.charAt(j)));
                 }
                 i++;
             } else if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == '.')) {
                 String stringNumber = format_string.substring(i+2);
-                try {
-                    int number = Integer.parseInt(stringNumber);
-                    //int length = String.valueOf(number).length();
-                    //if(format_string.charAt(i+1+length) == 'k') {
-                        for(int j=0;j<number;j++) {
+                int number = getNumber(stringNumber);
+                int length = String.valueOf(number).length();
+                if(number == -1) {
+                    System.out.print(format_string.charAt(i));
+                } else {
+                    //System.out.print("{number: " + number + ", length: " + length + "}");
+                    if(format_string.charAt(i+2+length) == 'k') {
+                        for (int j = 0; j < number; j++) {
                             System.out.print(swapLetterCase(param.charAt(j)));
                         }
-                    //}
-                } catch (NumberFormatException e) {
-                    System.out.print(format_string.charAt(i));
+                        i = i + 2 + length;
+                    } else {
+                        System.out.print(format_string.charAt(i));
+                    }
                 }
             }
             else{
                 System.out.print(format_string.charAt(i));
             }
         }
-        System.out.println("");
+        System.out.println();
     }
 
     private static char swapLetterCase(char letter) {
@@ -37,6 +40,20 @@ class lab0 {
             return Character.toUpperCase(letter);
         }
         return letter;
+    }
+
+    private static int getNumber(String argument) {
+        int number = -1;
+        for(int i=0;i<argument.length();i++) {
+            if(i==0) number = 0;
+            if(Character.isDigit(argument.charAt(i))) {
+                number *= 10;
+                number += Character.digit(argument.charAt(i), 10);
+            } else {
+                break;
+            }
+        }
+        return number;
     }
 
     public static void main(String[] args) throws IOException {
