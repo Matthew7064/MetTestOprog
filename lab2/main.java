998fd2ef5e690b2e3e396435d1094d7ed7c2977c
Matthew7064 <matthew.programowanie1@gmail.com>
2023-03-06 17:00:50 +0100

    Run tests

--- lab2/main.java
+++ lab2/main.java
@@ -4,10 +4,13 @@ class lab0 {
     public static void my_printf(String format_string, String param){
         for(int i=0;i<format_string.length();i++){
             if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-                System.out.print(param);
+                //System.out.print(param);
+                for(int j=0;j<param.length();j++) {
+		    System.out.print(swapLetterCase(param.charAt(j)));                
+                }
                 i++;
             }else{
-                System.out.print(swapLetterCase(format_string.charAt(i)));
+                System.out.print(format_string.charAt(i));
             }
         }
         System.out.println("");
